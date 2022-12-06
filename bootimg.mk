bf78fb01a492f488dfef94152cc58375c3462d4c
SamarV-121 <samarvispute121@pm.me>
2022-12-06 09:13:13 +0100

    m51: mkbootimg: Update for S
     * handling of kernel bootimg arg has been changed as of https://android-review.googlesource.com/c/platform/build/+/1394339
    
    Change-Id: Iba9d3e31621cffdeec9233fd90526baf89ded3e7
    Signed-off-by: SamarV-121 <samarvispute121@pm.me>

--- bootimg.mk
+++ bootimg.mk
@@ -4,7 +4,7 @@ FLASH_IMAGE_TARGET ?= $(PRODUCT_OUT)/recovery.tar
 
 $(INSTALLED_BOOTIMAGE_TARGET): $(MKBOOTIMG) $(INTERNAL_BOOTIMAGE_FILES) $(BOOTIMAGE_EXTRA_DEPS)
 	$(call pretty,"Target boot image: $@")
-	$(hide) $(MKBOOTIMG) $(INTERNAL_BOOTIMAGE_ARGS) $(INTERNAL_MKBOOTIMG_VERSION_ARGS) $(BOARD_MKBOOTIMG_ARGS) --output $@
+	$(hide) $(MKBOOTIMG) --kernel $(call bootimage-to-kernel,$(1)) $(INTERNAL_BOOTIMAGE_ARGS) $(INTERNAL_MKBOOTIMG_VERSION_ARGS) $(BOARD_MKBOOTIMG_ARGS) --output $@
 	$(hide) echo -n "SEANDROIDENFORCE" >> $@
 	@echo "Made boot image: $@"
 
